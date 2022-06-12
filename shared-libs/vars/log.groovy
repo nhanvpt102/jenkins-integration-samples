2bcbd319fbb7352c1e2c3f6998caa361f12cba2b
NhanVo <nhanvtp102@gmail.com>
2022-06-12 17:14:09 +0700

    test

--- shared-libs/vars/log.groovy
+++ shared-libs/vars/log.groovy
@@ -1,25 +1,17 @@
 #!/usr/bin/env groovy
 
 def info(message) {
-  COLOR_INFO="\033[1;36m"
-  COLOR_RESET="\033[0m"
-  echo "${COLOR_INFO}INFO: ${message}${COLOR_RESET}"
+  echo "[INFO] ${message}"
 }
 
 def warning(message) {
-  COLOR_WARN="\033[1;93m"
-  COLOR_RESET="\033[0m"
-  echo "${COLOR_WARN}WARNING: ${message}${COLOR_RESET}"
+  echo "[WARNING] ${message}"
 }
 
 def error(message) {
-  COLOR_ERROR="\033[1;31m"
-  COLOR_RESET="\033[0m"
-  echo "${COLOR_ERROR}ERROR: ${message}${COLOR_RESET}"
+  echo "[ERROR] ${message}"
 }
 
 def pass(message) {
-  COLOR_PASS="\033[1;32m"
-  COLOR_RESET="\033[0m"
-  echo "${COLOR_PASS}PASS: ${message}${COLOR_RESET}"
+  echo "[PASS] ${message}"
 }
