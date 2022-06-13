c69c94e0b4b8c80ead6006fb40f2e4dbc976d1dd
NhanVo <nhanvtp102@gmail.com>
2022-06-13 20:47:33 +0700

    Add

---
+++ kubernetes-jenkins/deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Step 1: Create a namespace 'devops-tools' for Jenkins."
+# kubectl create namespace devops-tools
+
+echo "Step 2: Create a serviceAccount 'jenkins-admin'"
+# kubectl apply -f serviceAccount.yaml
+
+echo "Step 3: Create volume 'jenkins-pv-claim'"
+# kubectl create -f volume.yaml
+
+echo "Step 4: Create a Deployment 'jenkins'"
+kubectl apply -f deployment.yaml
+
+#Step 5: Create service.yaml
+kubectl apply -f service.yaml
+
+# kubectl -n devops-tools port-forward svc/jenkins-service 8080:8080 &>/tmp/jenkins8080.log &
+# VPT123vpt!
